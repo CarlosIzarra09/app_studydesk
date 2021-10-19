0ca108e61e23a346874681e6a6e564c44cd0e9ba
CarlosIzarra09 <eduizarra2@hotmail.com>
2021-10-18 21:41:10 -0500

    feat: add home page with navigation buttons

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -2,9 +2,41 @@ import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
+  final String user_name = 'Josias';
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Bienvenido, $user_name'),
+      ),
+      body: ListView(
+        children: <Widget>[
+          _routeButtons(context),
+        ],
+      ),
+    );
+  }
+
+  Widget _routeButtons(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        ElevatedButton(
+          child: const Text('ver documentos'),
+          onPressed: (){
+            //TODO: De esta manera me muevo a una ruta con nombre
+            //Navigator.of(context).pushNamed('/home');
+          }, ),
+
+        const Expanded(child: SizedBox()),
+
+        ElevatedButton(
+          child: const Text('buscar documentos'),
+          onPressed: (){
+            //Navigator.of(context).pushNamed('/home');
+
+          }, ),
+      ],
+    );
   }
 }
