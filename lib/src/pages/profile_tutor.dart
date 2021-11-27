db3981ea68746affe07c2242a416ceffe8eb0727
Josias <50959958+SirJoc@users.noreply.github.com>
2021-11-27 00:14:05 -0500

    update

--- lib/src/pages/profile_tutor.dart
+++ lib/src/pages/profile_tutor.dart
@@ -122,7 +122,7 @@ class _ProfileTutorPage extends State<ProfileTutorPage> {
 
                             SizedBox(height: 8,),
                             Text("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-                              style: TextStyle(fontFamily: "Doris-P", fontSize: 15),
+                              style: TextStyle(fontSize: 16),
                             ),
 
                           ],
@@ -136,12 +136,12 @@ class _ProfileTutorPage extends State<ProfileTutorPage> {
                         child: Column(
                           children: <Widget>[
                             Text("Correo", style: TextStyle(fontWeight: FontWeight.w700,
-                                fontFamily: "Doris-P", fontSize: 18
+                                fontFamily: "Doris-P", fontSize: 22
                             ),),
 
                             SizedBox(height: 8,),
                             //for list of clients
-                            Text("tutor@correo.com", style: TextStyle(fontFamily: "Doris-P", fontSize: 16
+                            Text("tutor@correo.com", style: TextStyle(fontSize: 18
                             ),),
 
                           ],
